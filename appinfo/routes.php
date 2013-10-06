2d902716fdda36ef7dbfffafd840b00d6c735958
bastei <bastei@users.noreply.github.com>
2013-10-06 21:31:52 +0200

    route for attachment prefetch images

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -344,9 +344,7 @@ $this->create('news_api_feeds_read', '/api/v1-2/feeds/{feedId}/read')->put()->ac
 
 $this->create('news_api_feeds_image', '/api/v1-2/feeds/{feedId}/{imageFile}')->get()->action(
 	function($params) {
-		header('Content-Type: image/jpeg');
-		header("X-Accel-Redirect: /data/owncloud/news/imgcache/".$params["feedId"]."/".$params["imageFile"] );
-		//return App::main('FeedAPI', 'getImage', $params, new DIContainer());
+		return App::main('FeedAPI', 'getImage', $params, new DIContainer());
 	}
 );
 
@@ -422,8 +420,6 @@ $this->create('news_api_items_unstar_multiple', '/api/v1-2/items/unstar/multiple
 
 $this->create('news_api_items_image', '/api/v1-2/items/{feedId}/{secretId}/{imageFile}')->get()->action(
 	function($params) {
-		header('Content-Type: image/jpeg');
-		header("X-Accel-Redirect: /data/owncloud/news/imgcache/".$params["feedId"]."/".$params["secretId"]."/".$params["imageFile"] );
-		//return App::main('ItemAPI', 'getImage', $params, new DIContainer());
+		return App::main('ItemAPI', 'getImage', $params, new DIContainer());
 	}
 );
