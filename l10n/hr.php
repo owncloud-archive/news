ad9a5df33bd84b11b1c7d88becee53c233158cf3
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-11 01:01:14 -0400

    [tx-robot] updated from transifex

--- l10n/hr.php
+++ l10n/hr.php
@@ -1,7 +1,10 @@
 <?php $TRANSLATIONS = array(
 "Address" => "Adresa",
 "Add" => "Dodaj",
+"New folder" => "Nova mapa",
 "Folder" => "mapa",
+"Folder name" => "Naziv mape",
+"from" => "od",
 "by" => "preko",
 "Download" => "Preuzimanje",
 "Cancel" => "Odustani",
