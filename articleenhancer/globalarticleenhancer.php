8a25b99279e306dc8f5e75d457a94551707d5fd5
Morris Jobke <hey@morrisjobke.de>
2014-09-28 23:28:39 +0200

    add fix which needs PHP 5.4.0 and libxml 2.7.8

--- articleenhancer/globalarticleenhancer.php
+++ articleenhancer/globalarticleenhancer.php
@@ -24,7 +24,13 @@ class GlobalArticleEnhancer implements ArticleEnhancer {
 	 */
 	public function enhance(Item $item) {
 		$dom = new \DOMDocument();
-		@$dom->loadHTML($item->getBody());
+		if(PHP_VERSION_ID >= 50400 && LIBXML_VERSION >= 20708) { // major * 10000 + minor * 1000 + release
+			// these options are available since 5.4.0
+			@$dom->loadHTML($item->getBody(), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+		} else {
+			// fallback
+			@$dom->loadHTML($item->getBody());
+		}
 		$xpath = new \DOMXpath($dom);
 
 		// remove youtube autoplay
@@ -45,11 +51,15 @@ class GlobalArticleEnhancer implements ArticleEnhancer {
 			}
 		}
 
-		// remove <!DOCTYPE
-		$dom->removeChild($dom->firstChild);
+		if(PHP_VERSION_ID < 50400 || LIBXML_VERSION < 20708) { // major * 10000 + minor * 1000 + release
+			// we need to do this, because the options to load HTML without them wasn't available
 
-		// remove <html><body></body></html>
-		$dom->replaceChild($dom->firstChild->firstChild->firstChild, $dom->firstChild);
+			// remove <!DOCTYPE
+			$dom->removeChild($dom->firstChild);
+
+			// remove <html><body></body></html>
+			$dom->replaceChild($dom->firstChild->firstChild->firstChild, $dom->firstChild);
+		}
 
 		// save all changes back to the item
 		$item->setBody(trim($dom->saveHTML()));
