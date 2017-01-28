f1797f08bcbbb1a8e6697fcbd0f6b22637c8ff42
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-01-28 01:01:16 +0100

    [tx-robot] updated from transifex

--- l10n/uk.js
+++ l10n/uk.js
@@ -115,6 +115,7 @@ OC.L10N.register(
     "Error when importing: file does not contain valid JSON" : "Помилка при імпорті: файл не містить дійсний JSON",
     "Help" : "Допомога",
     "Keyboard shortcuts" : "Сполучення клавіш",
-    "Documentation" : "Документація"
+    "Documentation" : "Документація",
+    "Report a bug" : "Повідомити про помилку"
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
